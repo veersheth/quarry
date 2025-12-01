c2ef501a1c54e7856ca50b43f58cabfe91f732ae
Veer <veerksheth@gmail.com>
2025-12-01 16:16:19 +1100

    add fallback icons

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -4,12 +4,14 @@
 
 {#if icon && (icon.startsWith("data:image") || icon.includes(".png") || icon.includes(".svg"))}
   <img class="item-icon" src={icon} alt="" />
-{:else}
+{:else if icon}
   <span class="item-icon">{icon}</span>
+{:else}
+  <div class="item-icon fallback"></div>
 {/if}
 
 <style>
-  img.item-icon {
+  img.item-icon, .item-icon.fallback {
     width: 20px;
     height: 20px;
   }
@@ -20,4 +22,9 @@
     margin-bottom: 6px;
   }
 
+  .item-icon.fallback {
+    border: 1px solid rgba(60, 60, 60, 1);
+    border-radius: 30%;
+  }
 </style>
+
